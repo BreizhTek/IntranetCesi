2d83a349aab4398cdfe97a91a05132ca0446269c
lorenzo <thelorenzo533@gmail.com>
2020-02-27 14:55:30 +0100

    Add bd.php file

--- controller/ControllerChat.php
+++ controller/ControllerChat.php
@@ -1,8 +1,14 @@
 <?php
 
+require_once "ressources/modele/ModelChat.php";
+
 class ControllerChat {
 
     public function index(){
+
+        $chat = new Chat();
+
+        require "./view/Chat/Default.php";
 
     }
 
