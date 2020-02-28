15c1c4d83e5a71db6767bd0b420efd168601de07
Quentin Grosdoigt <grosdoigtquentin35@gmail.com>
2020-02-28 11:29:01 +0100

    enregistrement et modification foncitonne pour le formulaire de la fiche utilisateur

--- ressources/modele/ModelUser.php
+++ ressources/modele/ModelUser.php
@@ -22,12 +22,86 @@ class ModelUser
         return $statement;
     }
 
-    public function getLastUser()
+    public function updateUser($data)
     {
-        // On simule une communication BDD
-        $return = new stdClass();
-        $return->nom = 'Titi';
-        $return->prenom = 'Toto';
-        return $return;
+        $db = database();
+        $sql = "UPDATE users
+                    SET
+                        users.Level = :level,
+                        users.Last_name = :last_name,
+                        users.First_name = :first_naeme,
+                        users.Birth = :birth,
+                        users.Post = :post,
+                        users.Phone = :phone,
+                        users.Address = :address,
+                        users.Tutor = :tutor,
+                        users.Tutor_mail = :tutor_mail,
+                        users.Mail = :mail,
+                        users.Password = :pwd
+                           WHERE users.Id = :id";
+        $statement = $db->prepare($sql);
+        $statement->bindValue(":id", $data['Id']);
+        $statement->bindValue(":level", $data['Level'],PDO::PARAM_INT);
+        $statement->bindValue(":last_name", $data['LastName'],PDO::PARAM_STR);
+        $statement->bindValue(":first_naeme", $data['FristName'],PDO::PARAM_STR);
+        $statement->bindValue(":birth", $data['Brith'],PDO::PARAM_STR);
+        $statement->bindValue(":post", $data['Post'],PDO::PARAM_STR);
+        $statement->bindValue(":phone", $data['Phone'],PDO::PARAM_INT);
+        $statement->bindValue(":address", $data['Adress'],PDO::PARAM_STR);
+        $statement->bindValue(":tutor", $data['NameTutor'],PDO::PARAM_STR);
+        $statement->bindValue(":tutor_mail", $data['MailTutor'],PDO::PARAM_STR);
+        $statement->bindValue(":mail", $data['Mail'],PDO::PARAM_STR);
+        $statement->bindValue(":pwd", $data['pwd'],PDO::PARAM_STR);
+        $statement->execute();
+        return $statement;
+    }
+
+    public function insertUser($data)
+    {
+        $db = database();
+        echo "<pre>";
+        var_dump($data);
+        $sql = "INSERT INTO users
+                    (
+                        Level,
+                        Last_name,
+                        First_name,
+                        Birth,
+                        Post,
+                        Phone,
+                        Address,
+                        Tutor,
+                        Tutor_mail,
+                        Mail,
+                        Password
+                    )
+                    VALUES
+                    (
+                        :level,
+                        :last_name,
+                        :first_naeme,
+                        :birth,
+                        :post,
+                        :phone,
+                        :address,
+                        :tutor,
+                        :tutor_mail,
+                        :mail,
+                        :pwd
+                    )";
+        $statement = $db->prepare($sql);
+        $statement->bindValue(":level", $data['Level'],PDO::PARAM_INT);
+        $statement->bindValue(":last_name", $data['LastName'],PDO::PARAM_STR);
+        $statement->bindValue(":first_naeme", $data['FristName'],PDO::PARAM_STR);
+        $statement->bindValue(":birth", $data['Brith'],PDO::PARAM_STR);
+        $statement->bindValue(":post", $data['Post'],PDO::PARAM_STR);
+        $statement->bindValue(":phone", $data['Phone'],PDO::PARAM_INT);
+        $statement->bindValue(":address", $data['Adress'],PDO::PARAM_STR);
+        $statement->bindValue(":tutor", $data['NameTutor'],PDO::PARAM_STR);
+        $statement->bindValue(":tutor_mail", $data['MailTutor'],PDO::PARAM_STR);
+        $statement->bindValue(":mail", $data['Mail'],PDO::PARAM_STR);
+        $statement->bindValue(":pwd", $data['pwd'],PDO::PARAM_STR);
+        $statement->execute();
+        return $statement;
     }
 }
