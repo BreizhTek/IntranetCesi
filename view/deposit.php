8b4fdce1e377be3a10d962a5ed0edf6270a086ef
unknown <contact@timothelaine.com>
2020-03-24 18:15:58 +0100

    up

--- view/deposit.php
+++ view/deposit.php
@@ -9,13 +9,13 @@
 </head>
 <body class=" lg:overflow-visible min-h-screen flex flex-col">
 
-<!------------------------ ERROR MESSAGE---------------------------------->
+<!------------------------ ERROR AND SUCCESS MESSAGE---------------------------------->
 <div  class="bg-teal-lightest border-t-4 border-teal rounded-b text-teal-darkest px-4 py-3 shadow-md my-2" >
 
-    <div class="flex font-mono font-bold" id="message">
+    <div class="flex flex-row font-mono font-bold">
 
         <svg class="h-6 w-6 text-teal mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
-
+        <p  class="ont-mono font-bold p-1 ml-3" id="message"></p>
     </div>
 
 </div>
